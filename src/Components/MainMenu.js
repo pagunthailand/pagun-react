17a6611104e322fec3422c26eca73d13e34774b2
Bom Chnanon <chanon.net@hotmail.com>
2023-03-09 23:21:52 +0700

    no message

--- src/Components/MainMenu.js
+++ src/Components/MainMenu.js
@@ -8,16 +8,60 @@ import HomeScreen from './HomeScreen';
 import ActionBarOption from './ActionBarOption';
 import ActionBarLogo from './ActionBarLogo';
 import Setting from './Setting';
+import Product from './Product';
+import History from './History';
+import Notication from './Notication';
+import User from './User';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createNativeStackNavigator();
-const MainMenu = () => {
+// const MainMenu = () => {
+//   return (
+//         //   <NavigationContainer>
+//         //   <Stack.Navigator>
+//         //     <Stack.Screen
+//         //       name="Home"
+//         //       component={HomeScreen}
+//         //       options={{
+//         //             title: '',
+//         //             headerStyle: {
+//         //                       backgroundColor: '#F6F6F6', //Set Header color
+//         //                     },
+                           
+//         //                     headerTitleStyle: {
+//         //                       fontWeight: 'bold',
+//         //                       alignItems: 'flex-start',
+//         //                     },
+//         //                     headerLeft: () => <ActionBarLogo />,
+//         //                     headerRight: () => <ActionBarOption />,
+                          
+//         //   }}
+//         //     ></Stack.Screen>
+//         //     <Stack.Screen name="Profile" component={ProfileScreen} />
+//         //     <Stack.Screen name="Setting" component={Setting} />
+//         //   </Stack.Navigator>
+//         // </NavigationContainer>
+
+//       //   <NavigationContainer>
+//       //   <Tab.Navigator screenOptions={{ headerShown: false }}>
+//       //     <Tab.Screen name="Product" component={ProductStackScreen} />
+//       //     <Tab.Screen name="History" component={HistoryStackScreen} />
+//       //     <Tab.Screen name="Notication" component={NoticationStackScreen} />
+//       //     <Tab.Screen name="User" component={UserStackScreen} />
+//       //   </Tab.Navigator>
+//       // </NavigationContainer>
+
+//       <></>
+
+//   )
+// }
+
+const ProductStack = createNativeStackNavigator();
+function ProductStackScreen() {
   return (
-          <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
+    <ProductStack.Navigator>
+      <ProductStack.Screen name="Product" component={Product}
+                    options={{
                     title: '',
                     headerStyle: {
                               backgroundColor: '#F6F6F6', //Set Header color
@@ -28,15 +72,50 @@ const MainMenu = () => {
                               alignItems: 'flex-start',
                             },
                             headerLeft: () => <ActionBarLogo />,
-                            headerRight: () => <ActionBarOption />,
-                          
-          }}
-            ></Stack.Screen>
-            <Stack.Screen name="Profile" component={ProfileScreen} />
-            <Stack.Screen name="Setting" component={Setting} />
-          </Stack.Navigator>
-        </NavigationContainer>
-  )
+                              headerRight: () => <ActionBarOption /> 
+                            }}
+        ></ProductStack.Screen>
+    </ProductStack.Navigator>
+  );
 }
 
-export default MainMenu
+const HistoryStack = createNativeStackNavigator();
+function HistoryStackScreen() {
+  return (
+    <HistoryStack.Navigator>
+      <HistoryStack.Screen name="History" component={History} />
+    </HistoryStack.Navigator>
+  );
+}
+
+const NoticationStack = createNativeStackNavigator();
+function NoticationStackScreen() {
+  return (
+    <NoticationStack.Navigator>
+      <NoticationStack.Screen name="Notication" component={Notication} />
+    </NoticationStack.Navigator>
+  );
+}
+
+const UserStack = createNativeStackNavigator();
+function UserStackScreen() {
+  return (
+    <UserStack.Navigator>
+      <UserStack.Screen name="User" component={User} />
+    </UserStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+export default function MainMenu() {
+  return (
+         <NavigationContainer>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="สินค้า" component={ProductStackScreen} />
+          <Tab.Screen name="ประวัติ" component={HistoryStackScreen} />
+          <Tab.Screen name="แจ้งเตือน" component={NoticationStackScreen} />
+          <Tab.Screen name="ผู้ใช้" component={UserStackScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+  );
+}
---
+++ src/Components/Notication.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Notication = () => {
+  return (
+    <View>
+      <Text>Notication</Text>
+    </View>
+  )
+}
+
+export default Notication
